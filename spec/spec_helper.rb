687c27356e9996837edfbf79512e0feedf76f282
mfittko <manuel.fittko@sofatutor.com>
2025-01-17 16:44:49 +0100

    Add CircleCI tools library to load path and require all Ruby files in it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 SimpleCov.start
 
+$LOAD_PATH.unshift(File.expand_path('../lib/circleci-tools', __dir__))
+Dir.glob(File.expand_path('../lib/circleci-tools/**/*.rb', __dir__)).each { |f| require f }
+
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
