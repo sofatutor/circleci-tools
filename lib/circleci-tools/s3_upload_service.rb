265d4b9ae9598fef64dd1eb33fa78cf3a48f8434
Manuel Fittko <manuel.fittko@sofatutor.com>
2025-01-17 12:40:18 +0100

    Add S3 upload service implementation

---
+++ lib/circleci-tools/s3_upload_service.rb
@@ -0,0 +1,20 @@
+require 'aws-sdk-s3'
+require 'logger'
+
+module Circleci
+  class S3UploadService
+    def initialize(bucket_name, logger: Logger.new(STDOUT))
+      @bucket_name = bucket_name
+      @logger = logger
+      @s3_client = Aws::S3::Client.new
+    end
+
+    def upload_file(file_path, s3_key)
+      @logger.info("Uploading #{file_path} to S3 bucket #{@bucket_name} with key #{s3_key}...")
+      @s3_client.put_object(bucket: @bucket_name, key: "circleci/#{s3_key}", body: File.read(file_path))
+      @logger.info("Uploaded #{file_path} to S3 bucket #{@bucket_name} with key #{s3_key}.")
+    rescue Aws::S3::Errors::ServiceError => e
+      @logger.error("Failed to upload #{file_path} to S3: #{e.message}")
+    end
+  end
+end
