beba3044b166ba9d3dab9e70922931062954da51
mfittko <manuel.fittko@sofatutor.com>
2025-01-21 19:40:42 +0100

    Enhance retry mechanism with configurable max retries and improved logging

--- lib/circleci-tools/retryable.rb
+++ lib/circleci-tools/retryable.rb
@@ -2,22 +2,28 @@ module CircleciTools
   module Retryable
     MAX_RETRIES = 5
     BACKOFF_FACTOR = 0.5
+    MAX_BACKOFF_TIME = 60
 
-    def with_retries
+    def with_retries(max_retries: MAX_RETRIES)
       retries = 0
       begin
         yield
       rescue => e
-        if retries < MAX_RETRIES
+        if retries < max_retries
           retries += 1
-          backoff_time = BACKOFF_FACTOR * (2 ** retries)
-          puts "Thread #{Thread.current.object_id}: Retry ##{retries} after #{backoff_time} seconds due to: #{e.message}"
+          backoff_time = [BACKOFF_FACTOR * (2 ** retries), MAX_BACKOFF_TIME].min.floor
+          retry_logger.info "Retry ##{retries} after #{backoff_time} seconds"
+          retry_logger.debug "Thread #{Thread.current.object_id}: Error: #{e.message}"
           sleep backoff_time
           retry
         else
-          puts "Thread #{Thread.current.object_id}: Error: #{e.message}"
+          retry_logger.warn "Thread #{Thread.current.object_id}: Error: #{e.message}"
         end
       end
+    end
+
+    def retry_logger
+      @logger ||= Logger.new(STDOUT)
     end
   end
 end
